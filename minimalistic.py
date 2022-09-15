b2b65aacefc0f8eec435db348ae6e65d9d34661a
LBJ-Nor <80926104+LBJ-Nor@users.noreply.github.com>
2022-09-15 14:39:24 +0200

    Update minimalistic.py

--- minimalistic.py
+++ minimalistic.py
@@ -1,18 +1,16 @@
 import random
-win_conditions = "rockpaperscissorsrock"
+
 choices = ["rock", "paper", "scissors"]
 cpu = random.choice(choices)
 
-p1 = input("Rock, Paper, Scissors: ").lower()
-while p1 not in choices:
+while (p1 := input("Rock, Paper, Scissors: ").lower()) not in choices:
     print("Invalid choice, try again!")
-    p1 = input("Rock, Paper, Scissors: ").lower()
 
 print(f"CPU picked: {cpu}")
 
 if p1 == cpu:
     print("Draw!")
-elif p1+cpu in win_conditions:
+elif p1+cpu in "rockpaperscissorsrock":
     print("CPU wins!")
 else:
     print("Player wins!")
